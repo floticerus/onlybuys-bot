28e38cbd1ef2b6ff97d86b1b4aa86116838907f0
Kevin von Flotow <vonflow@gmail.com>
2022-03-12 04:12:06 -0700

    copy whole cli folder

--- packages/electron/build-scripts/copy-cli.js
+++ packages/electron/build-scripts/copy-cli.js
@@ -1,17 +1,17 @@
 /* eslint-disable  @typescript-eslint/no-var-requires */
 const fs = require('fs')
-// const fsExtra = require('fs-extra')
+const fsExtra = require('fs-extra')
 const path = require('path')
 
 module.exports = (buildPath, electronVersion, platform, arch, done) => {
-  const source = path.join(__dirname, '../', '../', 'cli', 'bundle.js')
+  const source = path.join(__dirname, '../', '../', 'cli')
   const dest = path.join(buildPath, 'cli')
 
   if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest, { recursive: true })
   }
 
-  fs.copyFileSync(source, path.join(dest, 'bundle.js'))
+  fsExtra.copySync(source, dest)
 
   done()
 }
